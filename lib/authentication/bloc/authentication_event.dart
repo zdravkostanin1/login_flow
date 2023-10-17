ab6325c49d2a5f98f235e8c1818c973c172dd47b
zdravkostanin1 <zdravko.stanin@gmail.com>
2023-10-17 17:47:22 +0300

    implementing authentication bloc, event and state

--- lib/authentication/bloc/authentication_event.dart
+++ lib/authentication/bloc/authentication_event.dart
@@ -3,3 +3,19 @@ part of 'authentication_bloc.dart';
 abstract class AuthenticationEvent extends Equatable {
   const AuthenticationEvent();
 }
+
+final class _AuthenticationStatusChanged extends AuthenticationEvent {
+  const _AuthenticationStatusChanged(this.status);
+
+  final AuthenticationStatus status;
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
+
+final class AuthenticationLogoutRequested extends AuthenticationEvent {
+  @override
+  // TODO: implement props
+  List<Object?> get props => throw UnimplementedError();
+}
